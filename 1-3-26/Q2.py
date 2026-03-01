daa3aee705829acba49e18a7cb02196331ad6438
Hirthik Balaji C <hirthikbalaji2006@gmail.com>
2026-03-01 13:32:12 +0530

    Add problem statement and solution for Swim in Rising Water

---
+++ 1-3-26/Q2.py
@@ -0,0 +1,26 @@
+import heapq
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        visited = [[False]*n for _ in range(n)]
+
+        # (time, x, y)
+        heap = [(grid[0][0], 0, 0)]
+        visited[0][0] = True
+
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        while heap:
+            time, x, y = heapq.heappop(heap)
+
+            # reached destination
+            if x == n - 1 and y == n - 1:
+                return time
+
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+
+                if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    new_time = max(time, grid[nx][ny])
+                    heapq.heappush(heap, (new_time, nx, ny))
